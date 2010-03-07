8c903dd4b88561652d18afd8279e235bf2bfd42d
Antonio Lenton <antoniolenton@gmail.com>
2010-03-07 11:02:53 -0300

    More borken stuff.

--- test.py
+++ test.py
@@ -1,18 +1,23 @@
 class Nota(object):
     names = ['C', 'C#', 'D', 'D#', 'E', 'F', 'G', 'G#', 'A', 'A#', 'B']
-    def name(self):
-        octava = self.height / 12
-        nota = self.height % 12
-        print "%s%s" % (octava, names[nota])
     def __init__(self, delay, height, duration):
         self.delay = delay
         self.height = height
         self.duration = duration
+    def name(self):
+        octava = self.height / 12
+        nota = self.height % 12
+        print "%s%s" % (octava, names[nota])
+    def __eq__(self, other):
+        return self.height == other.height
 
+    def sounding_at(self, delay):
+        return self.delay <= delay <= self.delay + self.duration
 
 class Partitura(object):
     def sounding_at(self, delay):
-        return [n for n in self.notes
+        return [n for n in self.notes if n.sounding_at(delay)]
+
     def __init__(self, notas):
         self.notes = notes
 
@@ -23,7 +28,9 @@ class TextView(object):
         self.partitura = partitura
 
     def print_next(self):
-        sonando = [n for n in self.notas if
+        sonando = set(self.partitura.sounding_at())
+        for n in range(24):
+            if 
 
 example = Partitura([Nota(0, 3, 300), Nota(400, 8, 300), Nota(800, 13, 600)])
 
