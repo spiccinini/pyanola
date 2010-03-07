76047dda6b69a71c14d18e44eee60e2ea4da6d90
Horacio Duran <hduran@machinalis.com>
2010-03-07 11:47:39 -0300

    New fantastic amazing glorious model and sounding cache system

--- test.py
+++ test.py
@@ -8,18 +8,33 @@ class Nota(object):
         octava = self.height / 12
         nota = self.height % 12
         print "%s%s" % (octava, names[nota])
+
     def __eq__(self, other):
         return self.height == other.height
 
     def sounding_at(self, delay):
         return self.delay <= delay <= self.delay + self.duration
+    
 
 class Partitura(object):
-    def sounding_at(self, delay):
-        return [n for n in self.notes if n.sounding_at(delay)]
+    def __init__(self, notes):
+        self.notes = {}
+        self.sounding_cache = set()
+        for note in notes:
+            if note.delay not in self.notes:
+                self.notes[note.delay] = set()
+            self.notes[note.delay].append(note)
 
-    def __init__(self, notas):
-        self.notes = notes
+    def sounding_at(self, delay):
+        for note in [trash for trash in xrange(len(self.sounding_cache)) if not self.sounding_cache[trash].sounding_at(delay)]:
+            self.notes.pop(note)
+        self.sounding_cache+=self._starts_at(delay)
+        return self.sounding_cache
+
+    def _starts_at(self, delay):
+        return self.note.get(delay, set())
+        
+
 
 
 class TextView(object):
@@ -32,7 +47,7 @@ class TextView(object):
         for n in range(24):
             if 
 
-example = Partitura([Nota(0, 3, 300), Nota(400, 8, 300), Nota(800, 13, 600)])
+example = Partitura([Nota(0, 3, 300), Nota(0, 5, 300), Nota(400, 8, 300), Nota(800, 13, 600)])
 
 def main():
     
